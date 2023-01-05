ff01b6b5fa47302c90e853006ba72f88b90d33d4
Your Name <gksjksjksjks@gmail.com>
2023-01-05 22:31:30 +0900

    day off 210105

--- GHAS_bulletinBoard/src/test/test.java
+++ GHAS_bulletinBoard/src/test/test.java
@@ -2,6 +2,8 @@ package test;
 
 import java.util.Calendar;
 
+import javax.swing.JFileChooser;
+
 import bases.BaseFrame;
 
 public class test extends BaseFrame {
@@ -10,24 +12,28 @@ public class test extends BaseFrame {
 		new test();
 	}
 
-	public test() {
-//		setFrame("test", 500, 500);
+	private JFileChooser jf;
 
-		Calendar time = Calendar.getInstance();
-		
-		System.out.println(time.get(Calendar.YEAR));
-		System.out.println(time.get(Calendar.MONTH + 1));
-		System.out.println(time.get(Calendar.DAY_OF_MONTH));
+	public test() {
+		setFrame("test", 500, 500);
+
+//		Calendar time = Calendar.getInstance();
+//		
+//		System.out.println(time.get(Calendar.YEAR));
+//		System.out.println(time.get(Calendar.MONTH + 1));
+//		System.out.println(time.get(Calendar.DAY_OF_MONTH));
 
 	}
 
 	@Override
 	public void make() {
+		jf = new JFileChooser();
+		
 	}
 
 	@Override
 	public void disign() {
-
+		center.add(jf);
 	}
 
 	@Override
