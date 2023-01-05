60298171f3b93bc3084c0c57059e909f61339d58
ksj <gksjksjksjks@gmail.com>
2023-01-05 11:44:10 +0900

    update contenForm : 작성자 이슈 해결

--- GHAS_bulletinBoard/windows/windows/ContentForm.java
+++ GHAS_bulletinBoard/windows/windows/ContentForm.java
@@ -5,6 +5,7 @@ import java.util.Vector;
 import bases.BaseBt;
 import bases.BaseFrame;
 import bases.BaseLb;
+import jdbc.DbManager;
 
 public class ContentForm extends BaseFrame {
 	private String n_no;
@@ -14,14 +15,23 @@ public class ContentForm extends BaseFrame {
 	private String n_mkdate;
 	private BaseBt updateBt;
 	private BaseBt deleteBt;
+	private DbManager db;
+	private Vector<Vector<String>> tmpData;
+	private String u_name;
 
 	public ContentForm(Vector<String> contentsData) {
+		db = new DbManager();
+
 		this.n_no = contentsData.get(0);
 		this.u_no = contentsData.get(1);
 		this.n_title = contentsData.get(2);
 		this.n_contents = contentsData.get(3);
 		this.n_mkdate = contentsData.get(4);
-
+		
+		tmpData = db.getData("SELECT * FROM ghas_notice.user where u_no = ?;", u_no);
+		
+		this.u_name = tmpData.get(0).get(3);
+		
 		setFrame(n_title, 500, 500);
 	}
 
@@ -34,7 +44,7 @@ public class ContentForm extends BaseFrame {
 	@Override
 	public void disign() {
 		top.addChild();
-		top.left.add(new BaseLb("작성자 : " + u_no));
+		top.left.add(new BaseLb("작성자 : " + u_name));
 		top.right.add(new BaseLb("작성일 : " + n_mkdate));
 		
 		center.add(new BaseLb(n_contents));
