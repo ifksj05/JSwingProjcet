bb11908904232ed17d4d8d075121c93ccb6d5e6c
ksj <gksjksjksjks@gmail.com>
2023-01-13 14:38:51 +0900

    sa project : 제목 수정 기능 추가

--- saProjcet/window/windows/UpdateContentsForm.java
+++ saProjcet/window/windows/UpdateContentsForm.java
@@ -6,6 +6,7 @@ import bases.BaseFrame;
 import bases.BaseJB;
 import bases.BaseJL;
 import bases.BaseTA;
+import bases.BaseTF;
 import jdbc.DbManager;
 import model.UserModel;
 import model.msg;
@@ -20,6 +21,7 @@ public class UpdateContentsForm extends BaseFrame {
 	private Vector<String> contentsArr;
 	private DbManager db;
 	private MainForm mainForm;
+	private BaseTF jtfTitle;
 
 	public UpdateContentsForm(Vector<String> contentsArr, MainForm mainForm) {
 		this.contentsArr = contentsArr;
@@ -31,6 +33,7 @@ public class UpdateContentsForm extends BaseFrame {
 	public void make() {
 		jlName = new BaseJL("작성자 : " + contentsArr.get(1)).setFont(15);
 		jlWritingDate = new BaseJL("작성 날짜 : " + contentsArr.get(4)).setFont(15);
+		jtfTitle = new BaseTF(contentsArr.get(2));
 		jtaContents = new BaseTA();
 		jtaContents.setText(contentsArr.get(3));
 
@@ -46,7 +49,9 @@ public class UpdateContentsForm extends BaseFrame {
 		jpTop.jpLeft.add(jlName);
 		jpTop.jpRight.add(jlWritingDate);
 
-		jpCenter.add(jtaContents);
+		jpCenter.addChild();
+		jpCenter.jpTop.add(jtfTitle);
+		jpCenter.jpCenter.add(jtaContents);
 
 		jpBottom.addChild();
 		jpBottom.jpRight.setFlow();
@@ -59,18 +64,20 @@ public class UpdateContentsForm extends BaseFrame {
 	@Override
 	public void event() {
 		db = new DbManager();
-		
+
 		// * 만약 로그인 한 계정이 타 계정 이라면 *
 		if (!contentsArr.get(1).equals(UserModel.u_name)) {
 			jtaContents.setEnabled(false);
 			jbUpdate.setEnabled(false);
 			jbDelete.setEnabled(false);
 		}
-		
+
 		jbUpdate.addActionListener(e -> {
+			String title = jtfTitle.getText();
 			String contents = jtaContents.getText();
 			String n_no = contentsArr.get(0);
-			db.setDb("UPDATE `sa_projectㄹ`.`notice` SET `n_contents` = ? WHERE (`n_no` = ?);", contents, n_no);
+			db.setDb("UPDATE `sa_project`.`notice` SET `n_title` = ? WHERE (`n_no` = ?);", title, n_no);
+			db.setDb("UPDATE `sa_project`.`notice` SET `n_contents` = ? WHERE (`n_no` = ?);", contents, n_no);
 
 			msg.info("수정 성공");
 			mainForm.setContentsTable();
